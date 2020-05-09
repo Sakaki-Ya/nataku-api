43838db8d6536d62d57639a71b562d99e59cb440
2012user <2012user@user-no-MacBook-Pro.local>
2020-05-09 12:29:03 +0900

    モデルとマイグレ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
     has_many :posts
+    mount_uploader :avatar, AvatarUploader
+    has_secure_password
+
+    validates :name, length: { minimum: 1, maximum: 10,
+        message: "Must be between 1 and 10 characters." }
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+        message: "Not a valid email address." }, uniqueness: true
+    validates :password, format: { with: /\A[a-z\d]{8,100}+\z/i,
+        message: "Must have at least 8 alphanumeric characters." }
 end
--- db/migrate/20200506015138_create_posts.rb
+++ db/migrate/20200506015138_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :type
-      t.string :content
+      t.string :type, null: false
+      t.string :content, null: false
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
