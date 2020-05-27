5e8701b8095652b1c10b8aac04ea4311ed63b388
2012user <2012user@user-no-MacBook-Pro.local>
2020-05-27 19:37:24 +0900

    attr_reader追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::API
-    attr_accessor :current_user
-
+    attr_reader :current_user
+    
     def auth_request
         header = request.headers["Authorization"]
         token = header.split(" ").last if header
         begin
             @decoded = JsonWebToken.decode(token)
-            @current_user = ApplicationController.new
-            @current_user.current_user = User.find(@decoded[:sub])
+            @current_user = User.find(@decoded[:sub])
         rescue ActiveRecord::RecordNotFound => e
             render json: { error: e.message }
         rescue JWT::DecodeError => e
